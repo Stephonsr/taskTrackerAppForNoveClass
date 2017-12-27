eccea98957ddafe3919e8ff7152c9ab2dae38283
Javaclass <Javaclass@15000549cpu.fec.org>
2017-12-27 15:26:18 -0600

    taskTracker

--- src/main/java/com/oosdclass/taskTrackerApp2/dao/UserDAO.java
+++ src/main/java/com/oosdclass/taskTrackerApp2/dao/UserDAO.java
@@ -1,5 +1,7 @@
 package com.oosdclass.taskTrackerApp2.dao;
 
-public interface UserDAO {
+import com.oosdclass.taskTrackerApp2.model.User;
 
+public interface UserDAO {
+	public User retrieveByUserName(String username);
 }
--- src/main/java/com/oosdclass/taskTrackerApp2/service/UserService.java
+++ src/main/java/com/oosdclass/taskTrackerApp2/service/UserService.java
@@ -1,5 +1,6 @@
 package com.oosdclass.taskTrackerApp2.service;
 
+import com.oosdclass.taskTrackerApp2.model.User;
 public interface UserService {
-
+	public boolean isUserValid(User user);
 }
